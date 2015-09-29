e386bb9722b89e3cdf9c45bf303f810c2243aa9f
Michele T <mishkanader@gmail.com>
2015-09-29 02:59:32 -1000

    Assignment 8
    Parts still missing, its 3AM the time of the anti-christ so i think i'm
    gonna pass out.  WIll probably try to fix this later.

---
+++ Assignments/A08/A08myScript.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Mishkanader on 9/28/2015.
+ */
+
+
+$(document).ready(function() {
+  $("#A1").hide();
+  $("#F1").click(function () {
+    $("#A1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#A2").hide();
+  $("#F2").click(function () {
+    $("#A2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#A3").hide();
+  $("#F3").click(function () {
+    $("#A3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#A4").hide();
+  $("#F4").click(function () {
+    $("#A4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#A5").hide();
+  $("#F5").click(function () {
+    $("#A5").toggle();
+  });
+});
+
+
+
+
